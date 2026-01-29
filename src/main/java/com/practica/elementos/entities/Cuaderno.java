08d2781a41196cd3c0d35c94a42a87cf383b2ef1
SoniaCastillo-cmd <scastillocurso@gmail.com>
2026-01-29 10:55:17 +0100

    entidad,repositorio y data.sql

---
+++ src/main/java/com/practica/elementos/entities/Cuaderno.java
@@ -0,0 +1,18 @@
+package com.practica.elementos.entities;
+
+
+import jakarta.persistence.*;
+
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Cuaderno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nombre;
+}
