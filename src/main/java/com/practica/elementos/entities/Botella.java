b9023c62f3d8d96295c3bc8afceb183a7274bb3a
PoliShMad <poolina.sh@gmail.com>
2026-01-29 10:55:18 +0100

    add entities, repositories, data

---
+++ src/main/java/com/practica/elementos/entities/Botella.java
@@ -0,0 +1,19 @@
+package com.practica.elementos.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+@Entity
+@Data
+
+public class Botella {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+}
---
+++ src/main/java/com/practica/elementos/repositories/BotellaRepository.java
@@ -0,0 +1,8 @@
+package com.practica.elementos.repositories;
+
+import com.practica.elementos.entities.Botella;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface BotellaRepository extends JpaRepository<Botella,Long>{
+}
