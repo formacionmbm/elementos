113b933901120770aac8e9cbfde317d945a6e832
SoniaCastillo-cmd <scastillocurso@gmail.com>
2026-01-29 13:48:59 +0100

    Interface e implementación

--- src/main/java/com/practica/elementos/controllers/CuadernoController.java
+++ src/main/java/com/practica/elementos/controllers/CuadernoController.java
@@ -1,7 +1,9 @@
 package com.practica.elementos.controllers;
 
 import com.practica.elementos.entities.Cuaderno;
+import com.practica.elementos.service.CuadernoService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +16,16 @@ import java.util.List;
 @Slf4j
 public class CuadernoController {
 
+    @Autowired
+    CuadernoService servicio;
 
     @GetMapping("/cuaderno")
-    public String listaCuaderno(Model model){
+    public String listaCuadernos(Model model){
         log.info("[listarCuaderno]");
-        List<Cuaderno> lista= List.of();
+        List<Cuaderno> lista= servicio.findAll();
         model.addAttribute("cuadernos",lista);
+        log.debug("[Cuadernos: lista:{}]", lista);
         return "cuaderno";
-
     }
+
 }
---
+++ src/main/java/com/practica/elementos/service/CuadernoServiceImpl.java
@@ -0,0 +1,21 @@
+package com.practica.elementos.service;
+
+import com.practica.elementos.entities.Cuaderno;
+import com.practica.elementos.repositories.CuadernoRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class CuadernoServiceImpl implements CuadernoService{
+
+    CuadernoRepository repository;
+
+
+    @Override
+    public List<Cuaderno> findAll() {
+        return repository.findAll();
+    }
+}
