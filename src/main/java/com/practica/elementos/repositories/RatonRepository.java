b8ba0b92428f10322f30668d9017e27f7513a28d
yulikof <kobra4beta@gmail.com>
2026-01-29 10:54:54 +0100

    entity y repository  data.sql

---
+++ src/main/java/com/practica/elementos/repositories/RatonRepository.java
@@ -0,0 +1,8 @@
+package com.practica.elementos.repositories;
+
+import com.practica.elementos.entities.Raton;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RatonRepository extends JpaRepository<Raton,Long> {
+
+}
