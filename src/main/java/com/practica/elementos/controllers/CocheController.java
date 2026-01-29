7bb7e958c47e9ff65571f7711e33a6bb1c792fd4
salah <salah@>
2026-01-29 12:43:57 +0100

    + controller

---
+++ src/main/java/com/practica/elementos/controllers/CocheController.java
@@ -0,0 +1,31 @@
+package com.practica.elementos.controllers;
+
+import com.practica.elementos.entities.Coche;
+import com.practica.elementos.repositories.CocheRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping
+@Slf4j
+public class CocheController {
+
+    @Autowired
+    private CocheRepository cocheRepository;
+
+    @GetMapping("/coche")
+    public String listaCoche(Model model) {
+        log.info("[listaCoche]");
+
+        List<Coche> lista = cocheRepository.findAll();
+                //List.of();
+        model.addAttribute("listaCoches", lista);
+        return "coche";
+    }
+}
