f978a83d43d953889dc27db3d0a5a492f33fc890
formacionmbm <formacionmbm@gmail.com>
2026-01-30 09:56:55 +0100

    Revert "Commit 1"
    This reverts commit 9be6c287716de1642232d0b4c101104a4daed7b3.

--- src/main/java/com/practica/elementos/repositories/BicicletaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.practica.elementos.repositories;
-
-import com.practica.elementos.entities.Bicicleta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BicicletaRepository extends JpaRepository<Bicicleta,Long> {
-}
